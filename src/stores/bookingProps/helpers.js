18b3f9b69b6c821253440f3f382bbb82d3ac9a6e
Learde <leardeleo@gmail.com>
2023-05-03 00:01:01 +0300

    Fix bug with limit days widget property

--- src/stores/bookingProps/helpers.js
+++ src/stores/bookingProps/helpers.js
@@ -3,7 +3,7 @@ import { bookingProps } from "./bookingProps.js";
 export const initBookingPropsStore = (props) => {
     Object.keys(bookingProps).forEach((key) => {
         if (key === "limitDays" && typeof props[key] === "number")
-            bookingProps[key] = props[key] - 2;
+            bookingProps[key] = props[key] - 1;
         else if (props[key] !== undefined) bookingProps[key] = props[key];
     });
 };
